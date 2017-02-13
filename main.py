22f22538243dce979695371318e52915f1713a9e
Mike Kroutikov <mkroutikov@innodata.com>
2017-02-13 12:46:50 -0500

    added template code for xml stats computation

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import re
 import glob
 import collections
 import csv
+import lxml.etree as et
 
 def parse_corpus(fname):
 
@@ -48,6 +49,30 @@ def stats_txt(fname, dictionary):
 
     return per_file
 
+def stats_xml(fname, dictionary):
+
+    with io.open(fname, 'rb') as f:
+        xml = et.fromstring(f.read())
+
+    per_file = collections.defaultdict(int)
+    for elt in xml.findall('.//word'):
+        text = elt.attrib['text']
+
+        for word in re.findall(r'\w+', text):
+            word = word.lower()
+
+            if re.match(r'\d+$', word):
+                per_file['numeric'] += 1
+            elif len(word) < 3:
+                per_file['short'] += 1
+            elif word in dictionary:
+                per_file['dictionary'] += 1
+            else:
+                per_file['non-dictionary'] += 1
+            per_file['total'] += 1
+
+    return per_file
+
 def merge(d1, *av):
     '''merge one or more dictionaries into the first one'''
     for d in av:
@@ -78,6 +103,30 @@ def file_stats_txt(glob_pattern, dictionary, output_fname):
             'name': 'Totals'
         }, totals))
 
+def file_stats_xml(glob_pattern, dictionary, output_fname):
+
+    totals = collections.defaultdict(int)
+    with io.open(output_fname, 'w', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=['name', 'numeric', 'short', 'dictionary', 'non-dictionary', 'total'])
+        writer.writeheader()
+
+        for fname in sorted(glob.glob(glob_pattern)):
+            name = os.path.basename(fname)
+            print('Processing:', name)
+
+            per_file = stats_xml(fname, dictionary)
+
+            for key, val in per_file.items():
+                totals[key] += val
+
+            writer.writerow(merge({
+                'name': name
+            }, per_file))
+
+        writer.writerow(merge({
+            'name': 'Totals'
+        }, totals))
+
 if __name__ == '__main__':
 
     print('Loading German dictionary...')
@@ -87,3 +136,4 @@ if __name__ == '__main__':
     print('Loaded', len(dictionary), 'unique lowercase words')
 
     file_stats_txt('ocr-hlsl/*.txt', dictionary, output_fname='ocr-hlsl.csv')
+    file_stats_xml('ocr-inno/*.xml', dictionary, output_fname='ocr-inno.csv')
